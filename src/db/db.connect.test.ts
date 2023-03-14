70ee2ddfa8c75e34c437e8a822ae668e13558432
Elena <ebromeo88@gmail.com>
2023-03-14 17:58:51 +0100

    Update dbconnect.test.ts file

--- src/db/db.connect.test.ts
+++ src/db/db.connect.test.ts
@@ -1,14 +1,23 @@
 import mongoose from 'mongoose';
-import { dbConnect } from './db.connect';
+import { dbConnect } from './db.connect.js';
 
-jest.mock('mongoose');
-
-describe('Given a dbconnect function', () => {
-  dbConnect();
-
-  describe('When its called', () => {
-    test('Then it should call the mongoose.connect method', () => {
-      expect(mongoose.connect).toHaveBeenCalled();
+describe('Given the dbConnect function ', () => {
+  describe('When NODE_ENV !== test ', () => {
+    test('Then it should be a connection to testing db', async () => {
+      mongoose.disconnect();
+      const result = await dbConnect();
+      expect(typeof result).toBe(typeof mongoose);
+      expect(mongoose.connection.db.databaseName).toContain('Testing');
+      mongoose.disconnect();
+    });
+  });
+  describe('When NODE_ENV !== test', () => {
+    test('Then it should be a connection to testing db', async () => {
+      mongoose.disconnect();
+      const result = await dbConnect('dev');
+      expect(typeof result).toBe(typeof mongoose);
+      expect(mongoose.connection.db.databaseName).not.toContain('Testing');
+      mongoose.disconnect();
     });
   });
 });
