c19f458e84020792af505f64bab9e40b85669870
Elena <ebromeo88@gmail.com>
2023-03-14 17:50:13 +0100

    Modify file to fixed sonar

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -1,15 +1,9 @@
 import mongoose from 'mongoose';
 import { config } from '../config.js';
-import createDebug from 'debug';
-
-const debug = createDebug('HOME:DBconnect');
 
 const { user, password, cluster, dbName } = config;
 
-export const dbConnect = (env?: string) => {
-  const finalEnv = env || process.env.NODE_ENV;
-  const finalDBName = finalEnv === 'test' ? dbName + '_Testing' : dbName;
-  const uri = `mongodb+srv://${user}:${password}@${cluster}/${finalDBName}?retryWrites=true&w=majority`;
-  debug(uri);
+export const dbConnect = () => {
+  const uri = `mongodb+srv://${user}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
   return mongoose.connect(uri);
 };
