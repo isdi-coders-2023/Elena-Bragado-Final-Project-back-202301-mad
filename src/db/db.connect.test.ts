e381f3c9469774d8a8f97d7170c77c251326f9fd
Elena <ebromeo88@gmail.com>
2023-03-14 14:15:20 +0100

    Modify test db-connect to get 100% amend lines 7&8

--- src/db/db.connect.test.ts
+++ src/db/db.connect.test.ts
@@ -1,16 +1,20 @@
+import { dbConnect } from './db.connect';
 import mongoose from 'mongoose';
-import { config } from '../config.js';
-import { dbConnect } from './db.connect.js';
 
-jest.mock('mongoose');
-
-describe('Given the dbConnect function', () => {
-  describe('When it is called is not test enviroments ', () => {
-    test('Then it should call the mongoose.connect and connect with uri', async () => {
-      const { user, password, cluster, dbName } = config;
-      const uri = `mongodb+srv://${user}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
-      await dbConnect('env');
-      expect(mongoose.connect).toHaveBeenCalledWith(uri);
+describe('Given dbConnect function', () => {
+  describe('When NODE_ENV === test', () => {
+    test('Then it should be a connection to testing db', async () => {
+      const result = await dbConnect();
+      expect(typeof result).toBe(typeof mongoose);
+      expect(mongoose.connection.db.databaseName).toContain('Testing');
+      mongoose.disconnect();
+    });
+  });
+  describe('When NODE_ENV !== test', () => {
+    test('Then it should be a connection to testing db', async () => {
+      const result = await dbConnect('dev');
+      expect(typeof result).toBe(typeof mongoose);
+      expect(mongoose.connection.db.databaseName).not.toContain('Testing');
       mongoose.disconnect();
     });
   });
