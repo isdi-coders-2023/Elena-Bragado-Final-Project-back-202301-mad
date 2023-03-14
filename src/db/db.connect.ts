ac904521845f2eeb54df60c6d65a56425efab3b5
Elena <ebromeo88@gmail.com>
2023-03-14 15:22:40 +0100

    Modify db.connect.ts file

--- src/db/db.connect.ts
+++ src/db/db.connect.ts
@@ -4,8 +4,8 @@ import { config } from '../config.js';
 const { user, password, cluster, dbName } = config;
 
 export const dbConnect = (env?: string) => {
-  const envFile = env || process.env.NODE_ENV;
-  const fileDBName = envFile === 'test' ? dbName + '_Testing' : dbName;
-  const uri = `mongodb+srv://${user}:${password}@${cluster}/${fileDBName}?retryWrites=true&w=majority`;
+  const envFinal = env || process.env.NODE_ENV;
+  const finalDBName = envFinal === 'test' ? dbName + '_Testing' : dbName;
+  const uri = `mongodb+srv://${user}:${password}@${cluster}/${finalDBName}?retryWrites=true&w=majority`;
   return mongoose.connect(uri);
 };
