f347af72ddd359523cfb56bd668d627e8fb52a10
Elena <ebromeo88@gmail.com>
2023-03-14 18:58:45 +0100

    Update db test

--- src/db/db.connect.test.ts
+++ src/db/db.connect.test.ts
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
 import { dbConnect } from './db.connect';
 
-jest.setTimeout(10000);
 describe('Given dbConnect function ', () => {
-  describe('When NODE_ENV !== test ', () => {
+  describe('When NODE_ENV === test ', () => {
     test('Then it should be a connection to testing db ', async () => {
-      mongoose.disconnect();
       const result = await dbConnect();
       expect(typeof result).toBe(typeof mongoose);
       expect(mongoose.connection.db.databaseName).toContain('Testing');
@@ -14,7 +12,6 @@ describe('Given dbConnect function ', () => {
   });
   describe('When NODE_ENV !== test', () => {
     test('Then it should be a connection to testing db', async () => {
-      mongoose.disconnect();
       const result = await dbConnect('dev');
       expect(typeof result).toBe(typeof mongoose);
       expect(mongoose.connection.db.databaseName).not.toContain('Testing');
