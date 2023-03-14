d3d6933a5236200a9f2206f06d69123ed9890805
Elena <ebromeo88@gmail.com>
2023-03-14 18:28:46 +0100

    Modify dbconnect test file

--- src/db/db.connect.test.ts
+++ src/db/db.connect.test.ts
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
-import { dbConnect } from './db.connect.js';
+import { dbConnect } from './db.connect';
 
+jest.setTimeout(5000);
 describe('Given dbConnect function ', () => {
   describe('When NODE_ENV !== test ', () => {
-    test('Then it should be a connection to testing db', async () => {
+    test('Then it should be a connection to testing db ', async () => {
       mongoose.disconnect();
       const result = await dbConnect();
       expect(typeof result).toBe(typeof mongoose);
