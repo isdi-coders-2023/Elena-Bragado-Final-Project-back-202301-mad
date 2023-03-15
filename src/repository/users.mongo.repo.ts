8b1197ed92454f0930e76807123f392737270b24
Elena <ebromeo88@gmail.com>
2023-03-15 17:48:23 +0100

    Add users mongo repo file

---
+++ src/repository/users.mongo.repo.ts
@@ -0,0 +1,32 @@
+import { User } from '../entities/user.js';
+import { Repo } from './users.repo.interface.js';
+import { UserModel } from './user.mongo.model.js';
+import createDebug from 'debug';
+
+const debug = createDebug('HOME:repo:users');
+
+export class UsersMongoRepo implements Repo<User> {
+  private static instance: UsersMongoRepo;
+
+  public static getInstance(): UsersMongoRepo {
+    if (!UsersMongoRepo.instance)
+      UsersMongoRepo.instance = new UsersMongoRepo();
+    return UsersMongoRepo.instance;
+  }
+
+  private constructor() {
+    debug('Instantiated at constructor');
+  }
+
+  async search(query: { key: string; value: unknown }): Promise<User[]> {
+    debug('Instantiated at constructor at search method');
+    const data = await UserModel.find({ [query.key]: query.value });
+    return data;
+  }
+
+  async create(info: Partial<User>): Promise<User> {
+    debug('Instantiated at constructor at create method');
+    const data = await UserModel.create(info);
+    return data;
+  }
+}
