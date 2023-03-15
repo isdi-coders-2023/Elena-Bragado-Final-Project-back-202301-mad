0849f5d754a2bfc9e5d53809bd8e6cccc3298d86
Elena <ebromeo88@gmail.com>
2023-03-15 17:47:32 +0100

    Modify jest.config with model.mongo.repo exclude

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,11 @@ export default {
   testPathIgnorePatterns: ['dist'],
   resolver: 'jest-ts-webcompat-resolver',
   collectCoverageFrom: ['src/**/*.ts'],
-  coveragePathIgnorePatterns: ['index.ts', 'app.ts', 'routers', 'config.ts'],
+  coveragePathIgnorePatterns: [
+    'index.ts',
+    'app.ts',
+    'routers',
+    'config.ts',
+    'user.mongo.model.ts',
+  ],
 };
