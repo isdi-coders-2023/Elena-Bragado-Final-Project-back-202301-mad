ef0d2603fde1c36e41ae3c9b2594902db21545c3
Elena <ebromeo88@gmail.com>
2023-03-15 23:19:29 +0100

    Modify users.mongo.repo.test.ts file

--- src/repository/users.mongo.repo.test.ts
+++ src/repository/users.mongo.repo.test.ts
@@ -6,25 +6,24 @@ jest.mock('./user.mongo.model');
 describe('Given UserMongoRepo', () => {
   const repoInstance = UsersMongoRepo.getInstance();
   describe('when we call getInstance()', () => {
-    test('then the UserMongoRepo shoul be instanciate', async () => {
+    test('then the UserMongoRepo shoul be INSTANTIATE', async () => {
       expect(repoInstance).toBeInstanceOf(UsersMongoRepo);
     });
   });
-
   describe('when the create method is used', () => {
     test('then it should return the new data', async () => {
-      const mockNewItem = { id: '1' };
+      const mockNewItem = { id: '2' };
       (UserModel.create as jest.Mock).mockResolvedValue(mockNewItem);
       const result = await repoInstance.create(mockNewItem);
       expect(UserModel.create).toHaveBeenCalled();
-      expect(result.id).toBe('1');
+      expect(result.id).toBe('2');
     });
   });
   describe('when the search method is used', () => {
     test('then it should return the data searched', async () => {
-      const mockItem = { id: '1' };
+      const mockItem = { id: '2' };
       (UserModel.find as jest.Mock).mockResolvedValue(mockItem);
-      const result = await repoInstance.search({ key: 'any', value: 'some' });
+      const result = await repoInstance.search({ key: 'some', value: 'oso' });
       expect(UserModel.find).toHaveBeenCalled();
       expect(result).toEqual(mockItem);
     });
